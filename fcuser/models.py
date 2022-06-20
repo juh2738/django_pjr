bf21a35910d1cb0328797abc93234e8a52f58325
yunho cho <juh2738@naver.com>
2022-06-20 23:16:32 +0900

    페이징 처리 까지

--- fcuser/models.py
+++ fcuser/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 class Fcuser(models.Model):
     username = models.CharField(max_length=64, verbose_name="사용자명")
-    useremail = models.EmailField(max_length=128, verbose_name="사용자이메일")
+    useremail = models.EmailField(max_length=128, verbose_name="사용자이메일",default='')
     password = models.CharField(max_length=64, verbose_name="비밀번호")
     register_dttm = models.DateTimeField(auto_now_add=True, verbose_name="등록시간")
 
